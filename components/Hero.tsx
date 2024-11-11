4c8eeffa9f71ea5be3a5ff3e878c62a6c25b4527
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-11-11 11:04:08 +0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero: FC = () => {
       <h1 className={"text-md text-center font-bold md:text-2xl xl:text-left lg:text-3xl xl:max-w-[500px] cursor-default"}>
         Showcase your keyboard skills, climb the leaderboard, and become the{" "}
         <span className={"bg-gradient-to-r from-[#ff00a9] to-[#ff6c00] text-transparent bg-clip-text"}>Fastest Writer Alive</span>
-        . You won't earn big money, but you will earn advert points and exchange it into realmoney,{" "}
+        . You earn passive income but not a big or doubled money , but you will earn advert points and exchange it into real cash,{" "}
         <Tooltip
           trigger={"to some, is more important!"}
           content={<Image src={RespectIsImportant} alt={"Respect is important"} className={"w-60"} />}
